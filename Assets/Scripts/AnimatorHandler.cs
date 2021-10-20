d7fb8ec9c5d0227a50ee7a1868af6d372ebb9409
Babi6k <yaniv.hayat1@gmail.com>
2021-10-20 22:10:40 +0300

    Added Animation Handler , made first animation of flipping card from player deck to display a card

---
+++ Assets/Scripts/AnimatorHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace WarcardProto
+{
+    public class AnimatorHandler : MonoBehaviour
+    {
+        Animator animator;
+
+        private void Awake() 
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void FlipCard()
+        {
+            gameObject.SetActive(true);
+            animator.SetBool("flip", true);
+        }
+    }
+}
--- Assets/Scripts/SpriteHandler.cs
+++ Assets/Scripts/SpriteHandler.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace WarcardProto
 {
     public class SpriteHandler : MonoBehaviour
     {
+        [Header("Models")]
         public List<Sprite> cardModels;
         public Sprite coverCardPlayer;
         public Sprite coverCardAI;
+
+        [Header("Display card positions")]
         public Transform displayPlayerCard;
         public Transform displayAICard;
+
+        [Header("Animations cards")]
+        public float waitAnimationTime = 0.5f;
+        public AnimatorHandler animatorHandler;
 
         /*The list is populated with the cards going from clubs to diamond see Suit enum for ref
         for example jack of hearts will be suit = 1 , rank = 9 
@@ -20,7 +28,21 @@ namespace WarcardProto
             return cardModels[(int)suit * 13 + rank];
         }
 
+        IEnumerator AnimateCards()
+        {
+            animatorHandler.FlipCard();
+            yield return new WaitForSeconds(waitAnimationTime);
+            animatorHandler.gameObject.SetActive(false);
+            ChangeCards();
+        }
+
+
         public void ShowCards()
+        {
+            StartCoroutine(AnimateCards());
+        }
+
+        private void ChangeCards()
         {
             Card playerCard = displayPlayerCard.GetComponentInChildren<Card>();
             Card AICard = displayAICard.GetComponentInChildren<Card>();
